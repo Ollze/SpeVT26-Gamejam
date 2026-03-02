10cbc7bfcaeadaca2250e0df78781fc680a2d235
vera <verasigurdson@icloud.com>
2026-03-02 14:48:07 +0100

    Update PlayerMain.cs

--- SpelVT26 GAMEJAM/Assets/OlleScripts/PlayerMain.cs
+++ SpelVT26 GAMEJAM/Assets/OlleScripts/PlayerMain.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerMain : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     float moveSpeed = 5;
     public ParticleSystem shootParticles;
+    public float MaxMana = 100f;
+    public float CurrentMana;
+    public Slider Mana;
+    public Slider playerHealth;
     void Start()
     {
-        
+        MaxMana = CurrentMana;
     }
 
     // Update is called once per frame
@@ -18,7 +23,7 @@ public class PlayerMain : MonoBehaviour
         if (Keyboard.current.sKey.isPressed) { transform.position += new Vector3(0, -1, 0)* moveSpeed * Time.deltaTime; }
         if (Keyboard.current.dKey.isPressed) { transform.position += new Vector3(1, 0, 0)* moveSpeed * Time.deltaTime; }
         if (Keyboard.current.aKey.isPressed) { transform.position += new Vector3(-1, 0, 0) * moveSpeed * Time.deltaTime; }
-        if (Mouse.current.leftButton.isPressed)
+        if (Mouse.current.leftButton.isPressed && CurrentMana > 0)
         {
             Shoot();
         }
@@ -28,5 +33,6 @@ public class PlayerMain : MonoBehaviour
     public void Shoot()
     {
         shootParticles.Play();
+        CurrentMana = -2f * Time.deltaTime;
     }
 }
