83b0c000db20eaf4264f60e0f7aea843046f7985
vera <verasigurdson@icloud.com>
2026-03-03 13:26:27 +0100

    ee

--- SpelVT26 GAMEJAM/Assets/MaxSkripts/PlayerHealth.cs
+++ SpelVT26 GAMEJAM/Assets/MaxSkripts/PlayerHealth.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public int maxHealth = 10;
+    public UnityEngine.UI.Slider healthBar;
      
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = health;
     }
 
    public void TakeDamage(int amount)
     {
         health -= amount;
+        healthBar.value = health;
         if (health <= 0)
         {
             Destroy(gameObject);
--- SpelVT26 GAMEJAM/Assets/VERAS/ShopUppgrades.cs
+++ SpelVT26 GAMEJAM/Assets/VERAS/ShopUppgrades.cs
@@ -5,6 +5,7 @@ public class ShopUppgrades : MonoBehaviour
     public GameObject player;
     public static float upgradedDamageAmount = 1;
     public PlayerMain playerCode;
+    bool istesting;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,13 +17,15 @@ public class ShopUppgrades : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
 
     public void DMGuppgrade()
     {
+
         upgradedDamageAmount += 1f;
+        print("button pressed");
         
     }
 
