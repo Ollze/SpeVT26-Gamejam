30766d3ed533a05073cbab2313b93fd955d54f86
Ollze <Ollze00@gmail.com>
2026-03-03 13:26:29 +0100

    skibidi

--- SpelVT26 GAMEJAM/Assets/OlleScripts/WaveController.cs
+++ SpelVT26 GAMEJAM/Assets/OlleScripts/WaveController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class WaveController : MonoBehaviour
 {
+    private EnemySpawner spawnerScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
