352634475daff62edf2ecee594f8de31b93cc904
Ollze <Ollze00@gmail.com>
2026-03-03 14:35:14 +0100

    mer enemies spawnar efter tid

--- SpelVT26 GAMEJAM/Assets/OlleScripts/EnemySpawner.cs
+++ SpelVT26 GAMEJAM/Assets/OlleScripts/EnemySpawner.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using Unity.XR.Oculus.Input;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,7 +8,7 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemyPrefab;
     public Transform enemySpawnPoint;
     //public Vector3 spawnPoint;
-    public float spawnRate = 2f;
+    public float spawnRate = 3f;
     public int killCount;
     private float spawnTimer;
 
@@ -15,22 +16,50 @@ public class EnemySpawner : MonoBehaviour
     {
         spawnTimer += Time.deltaTime;
         
-
+        //dehär är progression systemet som gör allt svårare over time, ganska scuffed men it does the job;
         if (spawnTimer >= spawnRate)
         {
-            SpawnEnemy();
+            if (killCount < 30) { SpawnEnemy(1); }
+            
             spawnTimer = 0f;
+            if(killCount > 10)
+            {
+                SpawnEnemy(2);
+                if (killCount > 20)
+                {
+                    spawnRate = 2f;
+                    if(killCount > 30)
+                    {
+                        SpawnEnemy(2);
+                        if(killCount > 50)
+                        {
+                            spawnRate = 1.5f;
+                            if(killCount > 200 && spawnRate >= 0.5f)
+                            {
+                                
+                                spawnRate -= 0.1f;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-    private void SpawnEnemy()
+    private void SpawnEnemy(int enemyIndex)
     {
         //roterar till nåt random
         float randomAngle = Random.Range(0f, 360f);
         transform.rotation = Quaternion.Euler(0f, 0f, randomAngle);
         //set spawn point till 10 ifrån spelaren och spawna
+         
         Vector3 spawnPoint = enemySpawnPoint.position;
-        Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
-        //print("enemy spawned at bearing " + randomAngle);      
+        GameObject spawnedEnemy = Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+        //print("enemy spawned at bearing " + randomAngle);
+        // vi kollar scripten på den spawnade enemien
+        EnemyMovement enemyScript = spawnedEnemy.GetComponent<EnemyMovement>();
+        //Och väljer vilken sorts enemy det ska va
+        if(enemyIndex == 1) { enemyScript.enemy1 = true; };
+        if(enemyIndex == 2) { enemyScript.enemy2 = true; };
     }
 
 
